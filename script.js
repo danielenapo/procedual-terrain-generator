ee6c2c9df0000f592b41de8fb23d801c6b546618
Daniele Napolitano <33985608+danielenapo@users.noreply.github.com>
2018-05-29 11:15:17 +0200

    seed shower

--- script.js
+++ script.js
@@ -5,20 +5,21 @@ function setup(){
 	height=500;
 	createCanvas(width, height);
 	noStroke();
-
+	seed=Math.floor(Math.random()*1000000);
+	noiseSeed(seed);
 
 	background(0);
-	resolution=13;
+	resolution=13; //risoluzione dell'mmagine(grandezza dei quadrati)
+	document.getElementById("seed").innerHTML="seed: "+seed;
 
-	for(var y=0; y<height; y+=resolution){
+	for(var y=0; y<height; y+=resolution){ //per ogni quadrato del canvas
 		for (var x=0; x<width; x+=resolution){
-			z=255*noise(x*0.01, y*0.01);
-			if(noise(x*0.01, y*0.01)<0.4)
+			z=255*noise(x*0.01, y*0.01); //genera un colore con perlin noise con valori x e y
+			if(noise(x*0.01, y*0.01)<0.4) //se il noise è minore di 0.4 ()genera valori tra 0 e 1), allora è acqua
 				fill(24, 64, 216);
-			else
+			else                         //altrimenti è terreno
 				fill(z-20, z+30, z-15);
-
-			rect(x, y, resolution, resolution);
+			rect(x, y, resolution, resolution); //disegna il quadrato
 		}
 	}
 
