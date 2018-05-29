3066f1b2fde8a9cab0d9b661d399a0d5ea43b54b
Daniele Napolitano <33985608+danielenapo@users.noreply.github.com>
2018-05-29 10:03:30 +0200

    water added

--- script.js
+++ script.js
@@ -1,50 +1,25 @@
 //INIZIALIZZAZIONI
 function setup(){
 	//canvas
-	width=400;
-	height=400;
+	width=1000;
+	height=500;
 	createCanvas(width, height);
 	noStroke();
 
-	//perlin noise
-	//seed=Math.random()*100;
-	//noiseSeed(seed);
-
-
-	//input
-	//contaInput=16;
 
 	background(0);
-	resolution=3;
+	resolution=13;
 
 	for(var y=0; y<height; y+=resolution){
 		for (var x=0; x<width; x+=resolution){
 			z=255*noise(x*0.01, y*0.01);
-			fill(z-10, z-10, z+10);
+			if(noise(x*0.01, y*0.01)<0.4)
+				fill(24, 64, 216);
+			else
+				fill(z-20, z+30, z-15);
+
 			rect(x, y, resolution, resolution);
 		}
 	}
 
-	//input
-	// if (keyIsDown(RIGHT_ARROW) && contaInput>15){
-	// 	seed+=10;
-	// 	contaInput=0;
-	// }
-	// else if (keyIsDown(LEFT_ARROW) && contaInput>=15){
-	// 	seed-=10;
-	// 	contaInput=0;
-	// }
-	// else if (keyIsDown(UP_ARROW)&& contaInput>15){
-	// 	seed++;
-	// 	contaInput=0;
-	// }
-	// else if(keyIsDown(DOWN_ARROW)&& contaInput>15){
-	// 	seed--;
-	// 	contaInput=0;
-	// }
-
-	//cambio seed
-	//noiseSeed(seed);
-
-	//noLoop();
 }
